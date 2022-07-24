c7690ad2c7ce50b65acc4942cf4e706fb89ce844
WizMe-M <timkin.moxim@mail.ru>
2022-07-24 18:22:55 +0300

    created ViewLocator.cs to resolve views from viewmodels

---
+++ Stellarity/Basic/ViewLocator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using ReactiveUI;
+
+namespace Stellarity.Basic;
+
+internal class ViewLocator : IDataTemplate, IViewLocator
+{
+    public IControl Build(object data)
+    {
+        var name = data.GetType().FullName!.Replace("ViewModel", "View");
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            return (Control)Activator.CreateInstance(type)!;
+        }
+
+        return new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public bool Match(object? data)
+    {
+        return data is IViewModelBase;
+    }
+
+    public IViewFor? ResolveView<T>(T? viewModel, string? contract = null)
+    {
+        if (viewModel is null) return null;
+        
+        var name = viewModel.GetType().FullName!.Replace("ViewModel", "View");
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            return (IViewFor)Activator.CreateInstance(type)!;
+        }
+
+        return null;
+    }
+}
