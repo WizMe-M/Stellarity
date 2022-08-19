f691f8fa77eb4f3aadd9133078c15fa2d2ae29f1
WizMe-M <timkin.moxim@mail.ru>
2022-08-19 21:09:48 +0300

    created LineSeparator user control

---
+++ Stellarity/UserControls/LineSeparator.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Stellarity.UserControls;
+
+public class LineSeparator : UserControl
+{
+    public static readonly StyledProperty<IBrush> ColorProperty =
+        AvaloniaProperty.Register<LineSeparator, IBrush>(nameof(Color), Brushes.Black);
+
+    public static readonly StyledProperty<double> ThicknessProperty =
+        AvaloniaProperty.Register<LineSeparator, double>(nameof(Thickness), 1);
+
+    public IBrush Color
+    {
+        get => GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
+
+    public double Thickness
+    {
+        get => GetValue(ThicknessProperty);
+        set => SetValue(ThicknessProperty, value);
+    }
+}
