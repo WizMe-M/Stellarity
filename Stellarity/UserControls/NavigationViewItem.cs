9ba650529b53cdebee2db18bad26ce7485ce4e21
WizMe-M <timkin.moxim@mail.ru>
2022-08-20 17:43:30 +0300

    own NavigationViewItem that provides drawing MD icons

---
+++ Stellarity/UserControls/NavigationViewItem.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+
+namespace Stellarity.UserControls;
+
+public class NavigationViewItem : Aura.UI.Controls.Navigation.NavigationViewItem
+{
+    public static readonly StyledProperty<string> IconValueProperty =
+        AvaloniaProperty.Register<NavigationViewItem, string>(nameof(IconValue));
+
+    public string IconValue
+    {
+        get => GetValue(IconValueProperty);
+        set => SetValue(IconValueProperty, value);
+    }
+}
