cdce86ab6f2e8cb0739b3ba01421f23499d4e523
WizMe-M <timkin.moxim@mail.ru>
2022-08-21 14:36:44 +0300

    extension to convert bytes into Bitmap

---
+++ Stellarity/Extensions/ByteExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace Stellarity.Extensions;
+
+public static class ByteExtensions
+{
+    /// <summary>
+    /// Converts image data to Bitmap
+    /// </summary>
+    /// <param name="data">Byte array of image's data</param>
+    /// <returns>Bitmap or null, if data was null</returns>
+    public static Bitmap? ToBitmap(this byte[]? data)
+    {
+        if (data is null) return null;
+
+        using var ms = new MemoryStream(data);
+        return new Bitmap(ms);
+    }
+}
