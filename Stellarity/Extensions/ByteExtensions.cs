79f397c5eaba379e55278082eadd7765fbb04a5a
WizMe-M <timkin.moxim@mail.ru>
2022-08-21 22:39:23 +0300

    byte[] <-> object convert

--- Stellarity/Extensions/ByteExtensions.cs
+++ Stellarity/Extensions/ByteExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Stellarity.Extensions;
 
@@ -16,5 +18,28 @@ public static class ByteExtensions
 
         using var ms = new MemoryStream(data);
         return new Bitmap(ms);
+    }
+
+    public static byte[] ToBytes<T>(this T obj)
+    {
+        if (obj is null) throw new InvalidOperationException("Convertable object was null");
+
+        var bf = new BinaryFormatter();
+        using var ms = new MemoryStream();
+#pragma warning disable SYSLIB0011
+        bf.Serialize(ms, obj);
+#pragma warning restore SYSLIB0011
+        return ms.ToArray();
+    }
+
+    public static T FromBytes<T>(this byte[] buffer)
+    {
+        if (buffer is null) throw new InvalidOperationException("Bytes was null");
+
+        using var ms = new MemoryStream(buffer);
+        var bf = new BinaryFormatter();
+#pragma warning disable SYSLIB0011
+        return (T)bf.Deserialize(ms);
+#pragma warning restore SYSLIB0011
     }
 }
