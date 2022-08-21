2ef27da5bc950401ba0c59e05e3fcbd95f2cf400
WizMe-M <timkin.moxim@mail.ru>
2022-08-21 13:31:56 +0300

    IAsyncImageLoader

---
+++ Stellarity/Basic/IAsyncImageLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Stellarity.Basic;
+
+/// <summary>
+/// Represents async image loader
+/// </summary>
+public interface IAsyncImageLoader
+{
+    Task<Bitmap> LoadAsync();
+}
