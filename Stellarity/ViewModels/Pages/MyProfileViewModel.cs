d229a503991f6e69709a6f278158f666cc7784e2
WizMe-M <timkin.moxim@mail.ru>
2022-08-25 21:06:05 +0300

    profile view model features

--- Stellarity/ViewModels/Pages/MyProfileViewModel.cs
+++ Stellarity/ViewModels/Pages/MyProfileViewModel.cs
@@ -2,6 +2,8 @@
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using Stellarity.Basic;
 using Stellarity.Database.Entities;
 using Stellarity.Extensions;
@@ -13,11 +15,25 @@ public class MyProfileViewModel : PageViewModel, IAsyncImageLoader
     public MyProfileViewModel(Account user)
     {
         User = user;
+
+        var canSendComment = this.WhenAnyValue(model => model.CommentText,
+            s => !string.IsNullOrWhiteSpace(s));
+        SendComment = ReactiveCommand.Create(() =>
+        {
+            var comment = new Comment(User, User, CommentText);
+            Comments.Add(comment);
+        }, canSendComment);
+        GoEditProfile = ReactiveCommand.Create(() => { });
     }
 
-    public Bitmap? Avatar { get; private set; }
     public Account User { get; }
+
+    [Reactive]
+    public Bitmap? Avatar { get; private set; }
+
+    [Reactive]
     public string CommentText { get; set; } = string.Empty;
+
     public ObservableCollection<Comment> Comments { get; } = new();
 
     public ICommand GoEditProfile { get; }
