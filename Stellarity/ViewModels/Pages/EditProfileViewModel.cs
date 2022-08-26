4ab1a7160ad774dd7e9d1fa63357ad8052ff7929
WizMe-M <timkin.moxim@mail.ru>
2022-08-26 22:31:12 +0300

    edit profile page

---
+++ Stellarity/ViewModels/Pages/EditProfileViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Stellarity.Basic;
+using Stellarity.Database.Entities;
+using Stellarity.Extensions;
+
+namespace Stellarity.ViewModels.Pages;
+
+public class EditProfileViewModel : ValidatableViewModelBase, IAsyncImageLoader
+{
+    private readonly Account _user;
+
+    public EditProfileViewModel(Account user)
+    {
+        _user = user;
+        Nickname = _user.Nickname ?? _user.Email;
+        AboutSelf = _user.About;
+
+        SaveChanges = ReactiveCommand.CreateFromTask(async () => { });
+        SetAvatar = ReactiveCommand.Create(() => { });
+        ChangePassword = ReactiveCommand.Create(() => { });
+    }
+
+    [Reactive]
+    public Bitmap? Avatar { get; private set; }
+
+    [Reactive]
+    public string Nickname { get; set; }
+
+    [Reactive]
+    public string? AboutSelf { get; set; }
+
+    public ICommand SaveChanges { get; }
+    public ICommand SetAvatar { get; }
+    public ICommand ChangePassword { get; }
+
+    public Task<Bitmap?> LoadAsync()
+    {
+        var bm = _user.Avatar?.Data.ToBitmap();
+        Avatar = bm ?? Avatar;
+        return Task.FromResult(bm);
+    }
+}
---
+++ Stellarity/Views/Pages/EditProfileView.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using Stellarity.ViewModels.Pages;
+
+namespace Stellarity.Views.Pages;
+
+public partial class EditProfileView : ReactiveUserControl<EditProfileViewModel>
+{
+    public EditProfileView()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public EditProfileView(EditProfileViewModel viewModel) : this()
+    {
+        ViewModel = viewModel;
+        this.WhenActivated(async d =>
+        {
+            // await ViewModel!.LoadAsync();
+        });
+    }
+}
