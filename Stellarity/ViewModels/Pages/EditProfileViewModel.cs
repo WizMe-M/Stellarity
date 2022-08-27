b006af437a2352d935e36ef714711b350489cb46
WizMe-M <timkin.moxim@mail.ru>
2022-08-27 20:34:07 +0300

    img load from disk

--- Stellarity/ViewModels/Pages/EditProfileViewModel.cs
+++ Stellarity/ViewModels/Pages/EditProfileViewModel.cs
@@ -1,6 +1,11 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Avalonia.Media.Imaging;
+using HanumanInstitute.MvvmDialogs;
+using HanumanInstitute.MvvmDialogs.FrameworkDialogs;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using Stellarity.Basic;
@@ -11,6 +16,12 @@ namespace Stellarity.ViewModels.Pages;
 
 public class EditProfileViewModel : ValidatableViewModelBase, IAsyncImageLoader
 {
+    /// <summary>
+    /// Viewmodel to resolve view for <see cref="IDialogService"/>
+    /// </summary>
+    private readonly MainViewModel _windowOwner = null!;
+
+    private readonly IDialogService _dialogService = null!;
     private readonly Account _user;
 
     public EditProfileViewModel(Account user)
@@ -19,9 +30,37 @@ public class EditProfileViewModel : ValidatableViewModelBase, IAsyncImageLoader
         Nickname = _user.Nickname ?? _user.Email;
         AboutSelf = _user.About;
 
-        SaveChanges = ReactiveCommand.CreateFromTask(async () => { });
-        SetAvatar = ReactiveCommand.Create(() => { });
+        SaveChanges = ReactiveCommand.CreateFromTask(async () =>
+        {
+            // Account.SaveChanges(user);
+        });
+
+        SetAvatar = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var settings = new OpenFileDialogSettings
+            {
+                Title = "Выберите изображение для аватара своего профиля",
+                InitialDirectory = Directory.GetDirectoryRoot(Assembly.GetExecutingAssembly().Location),
+                Filters = new List<FileFilter>(new[]
+                {
+                    new FileFilter("Изображения", new[] { "jpg", "jpeg", "png" })
+                })
+            };
+
+            var imgPath = await _dialogService.ShowOpenFileDialogAsync(_windowOwner, settings);
+            if (imgPath is { })
+            {
+                Avatar = new Bitmap(imgPath);
+            }
+        });
         ChangePassword = ReactiveCommand.Create(() => { });
+    }
+
+
+    public EditProfileViewModel(IDialogService dialogService, MainViewModel windowOwner, Account user) : this(user)
+    {
+        _dialogService = dialogService;
+        _windowOwner = windowOwner;
     }
 
     [Reactive]
