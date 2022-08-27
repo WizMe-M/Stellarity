876d6b15f2d3cb5931f12337c125292b3d7c01f1
WizMe-M <timkin.moxim@mail.ru>
2022-08-27 20:41:33 +0300

    edit profile in navigation view

--- Stellarity/Views/MainView.axaml.cs
+++ Stellarity/Views/MainView.axaml.cs
@@ -1,7 +1,7 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using HanumanInstitute.MvvmDialogs.Avalonia;
 using Ninject;
 using ReactiveUI;
 using Stellarity.Services.Accounting;
@@ -21,6 +21,11 @@ public partial class MainView : ReactiveWindow<MainViewModel>
             var accountingService = App.Instance.DiContainer.Get<AccountingService>();
             Profile = this.GetControl<NavigationViewItem>(nameof(Profile));
             Profile.Content = new MyProfileView(new MyProfileViewModel(accountingService));
+
+            var dialogService = App.Instance.DiContainer.Get<DialogService>();
+            EditProfile = this.GetControl<NavigationViewItem>(nameof(EditProfile));
+            EditProfile.Content =
+                new EditProfileView(new EditProfileViewModel(dialogService, ViewModel!, accountingService.AuthorizedAccount!));
         });
 
         InitializeComponent();
