a19648e4f09c87b4f941d4f770a2e904c15e7808
WizMe-M <timkin.moxim@mail.ru>
2022-08-28 23:24:32 +0300

    fixed DialogService

--- Stellarity/Views/MainView.axaml.cs
+++ Stellarity/Views/MainView.axaml.cs
@@ -2,7 +2,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
-using HanumanInstitute.MvvmDialogs.Avalonia;
+using HanumanInstitute.MvvmDialogs;
 using Ninject;
 using ReactiveUI;
 using Stellarity.Services.Accounting;
@@ -23,7 +23,7 @@ public partial class MainView : ReactiveWindow<MainViewModel>
             Profile = this.GetControl<NavigationViewItem>(nameof(Profile));
             Profile.Content = new MyProfileView(new MyProfileViewModel(accountingService));
 
-            var dialogService = App.Current.DiContainer.Get<DialogService>();
+            var dialogService = App.Current.DiContainer.Get<IDialogService>();
             EditProfile = this.GetControl<NavigationViewItem>(nameof(EditProfile));
             EditProfile.Content =
                 new EditProfileView(new EditProfileViewModel(dialogService, ViewModel!, accountingService.AuthorizedAccount!));
