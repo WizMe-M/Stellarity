41e9928291fb503ba514ba6f43c5b72a03663a2f
WizMe-M <timkin.moxim@mail.ru>
2022-08-28 14:37:58 +0300

    created ImageButton control to make image-containing btn

---
+++ Stellarity/UserControls/ImageButton.axaml.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+
+namespace Stellarity.UserControls;
+
+public class ImageButton : Button
+{
+    public static readonly StyledProperty<IBitmap> ImageSourceProperty = AvaloniaProperty.Register<ImageButton, IBitmap>(
+        nameof(ImageSource));
+
+    public static readonly StyledProperty<Stretch> StretchProperty = AvaloniaProperty.Register<ImageButton, Stretch>(
+        nameof(Stretch), Stretch.UniformToFill);
+    
+    public static readonly StyledProperty<BoxShadows> BoxShadowProperty =
+        Border.BoxShadowProperty.AddOwner<ImageButton>();
+    public IBitmap ImageSource
+    {
+        get => GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+
+    public Stretch Stretch
+    {
+        get => GetValue(StretchProperty);
+        set => SetValue(StretchProperty, value);
+    }
+    
+    public BoxShadows BoxShadow
+    {
+        get => GetValue(BoxShadowProperty);
+        set => SetValue(BoxShadowProperty, value);
+    }
+}
