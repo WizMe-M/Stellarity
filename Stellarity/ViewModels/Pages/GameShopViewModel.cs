90acabdb2ffe114cda082c58eb5ea19cec842cab
WizMe-M <timkin.moxim@mail.ru>
2022-09-04 01:09:34 +0300

    blank of GameShop view and vm

---
+++ Stellarity/ViewModels/Pages/GameShopViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using DynamicData;
+using Stellarity.Basic;
+using Stellarity.Database.Entities;
+using Stellarity.ViewModels.WrapViewModels;
+
+namespace Stellarity.ViewModels.Pages;
+
+public class GameShopViewModel : IAsyncImageLoader
+{
+    public GameShopViewModel()
+    {
+        var games = Game.GetAll();
+        AllGames.AddRange(games.Select(g => new GameViewModel(g)));
+    }
+
+    public ObservableCollection<GameViewModel> AllGames { get; } = new();
+    public async Task LoadAsync()
+    {
+        foreach (var game in AllGames) await game.LoadAsync();
+    }
+}
---
+++ Stellarity/Views/Pages/GameShopView.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using Stellarity.ViewModels.Pages;
+
+namespace Stellarity.Views.Pages;
+
+public partial class GameShopView : ReactiveUserControl<GameShopViewModel>
+{
+    public GameShopView() => AvaloniaXamlLoader.Load(this);
+
+    public GameShopView(GameShopViewModel gameShopViewModel) : this()
+    {
+        ViewModel = gameShopViewModel;
+
+        this.WhenActivated(async d =>
+        {
+            await ViewModel.LoadAsync();
+        });
+    }
+}
