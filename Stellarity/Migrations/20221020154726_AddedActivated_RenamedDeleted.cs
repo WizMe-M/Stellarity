d28ebd1c5a882f1546d64c50123c61c3d15d6fa0
WizMe-M <timkin.moxim@mail.ru>
2022-10-20 18:48:04 +0300

    migrated db

---
+++ Stellarity/Migrations/20221020154726_AddedActivated_RenamedDeleted.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stellarity.Migrations
+{
+    public partial class AddedActivated_RenamedDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "deleted",
+                table: "users",
+                newName: "banned");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "activated",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "activated",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "banned",
+                table: "users",
+                newName: "deleted");
+        }
+    }
+}
